5501f7e0f19dfaa4966767d47cbbf9dadd2226a5
Naja Info <Naja Info@NALDINHO>
2022-09-03 20:03:54 -0300

    Adicionar arquivos de projeto.

---
+++ Aluno.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3
+{
+    internal class Aluno 
+    {
+        // Declaração de Atributo
+
+        string nome;
+        string cpf;
+        string endereco;
+        int ra;
+
+        // Declaração de Método setter e getter (Set e Get)
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome; // "this" refêrencia ao próprio objeto da classe
+        }
+        public string GetNome()
+        {
+            return this.nome; // retorna o valor 
+        }
+        public void SetCpf(string cpf)
+        {
+            this.cpf = cpf;
+        }
+        public string GetCpf()
+        {
+            return this.cpf;
+        }
+        public void SetEndereco(string endereco)
+        {
+            this.endereco = endereco;
+        }
+        public string GetEndereco()
+        {
+            return this.endereco;
+        }
+        public void SetRa(int ra)
+        {
+            this.ra = ra;
+        }
+        public int GetRa()
+        {
+            return this.ra;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarção e instânciaçãao dos objetos
+
+            Aluno al = new Aluno();
+            Mensalidade men = new Mensalidade();
+            // Declaração      =   Instânciação
+
+            // Chamada dos métodos
+
+            Console.Write("Digite o nome: ");
+            al.SetNome(Console.ReadLine());          // "Set" é para receber um valor
+            Console.Write("Digite o endereço: ");
+            al.SetEndereco(Console.ReadLine());
+            Console.Write("Digite o CPF: ");
+            al.SetCpf(Console.ReadLine());
+            Console.Write("Digite o RA: ");
+            al.SetRa(Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Digite o Curso: ");
+            men.SetCurso(Console.ReadLine());
+            Console.Write("Digite o valor: ");
+            men.SetValor(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Digite o dia do pagamento: ");
+            men.SetDia(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("\nDados do Aluno");
+            Console.WriteLine("Ra:" + al.GetRa() + "\nAluno: " + al.GetNome());
+            Console.WriteLine("Endereço: " + al.GetEndereco() + "\nCPF: " + al.GetCpf());
+            Console.WriteLine("Curso: " + men.GetCurso() + "\nValor da mensalidade: " + men.GetValor().ToString("C") +
+                              "\nValor a pagar: " + men.Calcular().ToString("C"));
+
+
+            Console.ReadKey();
+        }
+    }
+}
